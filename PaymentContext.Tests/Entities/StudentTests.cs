56002218a70d820397bf6fbc7049da22aed2d2f5
Igor Cerutti <igor.cpv2@gmail.com>
2023-07-25 21:44:02 -0300

    Adicionando regras de negocio Payment

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void AdicionarAssinatura()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("André", "Baltieri", "1234567", "balta@io");
+        student.AddSubscription(subscription);
+    }
+}
