daf60891feada4803966ae62c2df445d1f38cc98
Igor Cerutti <igor.cpv2@gmail.com>
2023-07-27 21:54:51 -0300

    Implementando Validações

--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -1,11 +1,16 @@
-﻿namespace PaymentContext.Domain.ValueObjects;
+﻿using PaymentContext.Shared.ValueObjects;
 
-public class Name
+namespace PaymentContext.Domain.ValueObjects;
+
+public class Name : ValueObject
 {
     public Name(string firstName, string lastName)
     {
         FirstName = firstName;
         LastName = lastName;
+        
+        if(string.IsNullOrEmpty(FirstName))
+            AddNotification("Name.FirstName", "Nome inválido");
     }
 
     public string FirstName { get; private set; }
