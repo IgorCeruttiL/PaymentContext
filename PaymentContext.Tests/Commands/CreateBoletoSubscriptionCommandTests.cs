cd12e5f0301b1c8be17667253ecfff7c13afbe89
Igor Cerutti <igor.cpv2@gmail.com>
2023-07-31 21:59:26 -0300

    Adicionado tests para os commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,29 @@
+﻿using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands;
+[TestClass]
+public class CreateBoletoSubscriptionCommandTests
+{
+    [TestMethod]
+    public void ShouldReturnErrorWhenNameIsInvalid()
+    {
+        var command = new CreateBoletoSubscriptionCommand();
+        command.FirstName = "";
+        command.LastName = "";
+        
+        command.Validate();
+        Assert.AreEqual(false, command.Valid);
+    }
+    
+    [TestMethod]
+    public void ShouldReturnErrorWhenNameIsValid()
+    {
+        var command = new CreateBoletoSubscriptionCommand();
+        command.FirstName = "igor";
+        command.LastName = "lima";
+        
+        command.Validate();
+        Assert.AreEqual(true, command.Valid);
+    }
+    
+}
